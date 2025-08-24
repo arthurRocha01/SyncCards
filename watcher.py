fabe94f39db0754f68a3e5d614cf5d52a8b8b6d6
kali <nmqvl@kali.kali>
2025-08-24 20:16:03 -0300

    feat: adciona captura de arquivos (imagens) em pares

--- watcher.py
+++ watcher.py
@@ -1,48 +1,26 @@
-"""
-Watcher de diretório usando watchdog.
-
-Objetivo:
-Monitorar a pasta ./inbox e processar automaticamente
-arquivos recém-criados através da função process_file.
-
-Componentes principais:
-- Handler: define ação quando um arquivo é criado.
-- start_watcher: inicia o observer e mantém a observação ativa.
-
-Uso:
-Chamando start_watcher(), o programa fica escutando
-novos arquivos no diretório e processa cada um automaticamente.
-"""
-
 import time
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 from service import process_file
 
-# Caminho do diretório a ser monitorado
 WATCH_PATH = "./inbox"
 
 class Handler(FileSystemEventHandler):
-    """
-    Handler de eventos de sistema de arquivos.
+    def __init__(self):
+        self.files_to_process = []
 
-    on_created:
-    Chamado automaticamente quando um arquivo é criado.
-    Ignora pastas e envia arquivos para a função process_file.
-    """
     def on_created(self, event):
         if not event.is_directory:
-            process_file(event.src_path)
+            self.files_to_process.append(event.src_path)
+            if len(self.files_to_process) == 2:
+                self.process_two_files()
+
+    def process_two_files(self):
+        files_pair = self.files_to_process[:2]
+        process_file(files_pair)
+        self.files_to_process = self.files_to_process[2:]
 
 def start_watcher():
-    """
-    Inicializa e inicia o watcher.
-
-    1. Cria um Handler para eventos.
-    2. Cria um Observer e conecta ao diretório WATCH_PATH.
-    3. Inicia a observação em loop contínuo.
-    4. Permite interrupção via Ctrl+C.
-    """
     event_handler = Handler()
     observer = Observer()
     observer.schedule(event_handler, WATCH_PATH, recursive=False)
